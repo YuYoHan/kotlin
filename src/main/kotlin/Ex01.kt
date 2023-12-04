6fdc6d817588c01645386e36b7cdc77fa5374002
YuYoHan <zxzz8014@naver.com>
2023-12-04 18:07:50 +0900

    [Study] 상수

--- src/main/kotlin/Ex01.kt
+++ src/main/kotlin/Ex01.kt
@@ -1,3 +1,6 @@
+// 탑 레벨 함수
+const val number = 7
+
 fun main() {
     // 코틀린은 별도의 타입을 지정하지 않아도 된다.
     var i = 10
@@ -8,4 +11,7 @@ fun main() {
     var i2 : Int = 10;
     var name2 : String = "민주"
     var point2 : Double = 3.3
+
+    // 상수
+    val num = 20
 }
