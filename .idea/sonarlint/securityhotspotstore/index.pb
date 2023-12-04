aa321dc2258ffbab3aec2e62c56497cac1116360
YuYoHan <zxzz8014@naver.com>
2023-12-04 18:01:48 +0900

    [Study] 변수선언

---
+++ .idea/sonarlint/securityhotspotstore/index.pb
@@ -0,0 +1,3 @@
+
+H
+src/main/kotlin/Hello.kt,e\3\e30d8e81980c204ce32444753966bf8cfe41c287
---
+++ src/main/kotlin/Ex01.kt
@@ -0,0 +1,11 @@
+fun main() {
+    // 코틀린은 별도의 타입을 지정하지 않아도 된다.
+    var i = 10
+    var name = "준석"
+    var point = 3.3
+
+    // 별도로 지정하는 방법
+    var i2 : Int = 10;
+    var name2 : String = "민주"
+    var point2 : Double = 3.3
+}
